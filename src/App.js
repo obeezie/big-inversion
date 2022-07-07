7eaf0e43d6d2cbfc11ec3b6e05f525ec44e499bb
Martin Vera <m.vera1996@gmail.com>
2022-07-07 15:42:46 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="John" lastName="Smith" age={8} hairColor="Brown" />
+      <PersonCard firstName="Sarah" lastName="Johnson" age={26} hairColor="Black" />
+      <PersonCard firstName="Martin" lastName="Vera" age={25} hairColor="Brown" />
+      <PersonCard firstName="Jessica" lastName="Navarro" age={24} hairColor="Green" />
     </div>
   );
 }
